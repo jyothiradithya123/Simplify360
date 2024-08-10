b181e34301dc5bd5dd0cdaa64676558b94583a0e
jyothiradithya123 <112580230+jyothiradithya123@users.noreply.github.com>
2024-08-10 15:11:53 +0530

    Added more comments

--- friends_network.py
+++ friends_network.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-friends_df = pd.DataFrame(columns=["friendA","friendB"]) #friend_df
+friends_df = pd.DataFrame(columns=["friendA","friendB"]) # a global df for storing friends data
 
+#adding new friendship for given two friends in bi-directional Eg: Alice->Bob, Bob->Alice
 def add_friendship(friend1,friend2):
     global friends_df
     new_friendship = pd.DataFrame({"friendA":[friend1],"friendB":[friend2]}) 
@@ -9,6 +10,7 @@ def add_friendship(friend1,friend2):
     new_reverse = pd.DataFrame({"friendA":[friend2],"friendB":[friend1]})
     friends_df = pd.concat([friends_df,new_reverse],ignore_index=True)
 
+#method for finding common friends for given two friends
 def find_common_friends(friend1,friend2):
     common_friends1 = friends_df.loc[(friends_df["friendA"] == friend1)]["friendB"]
     common_friends2 = friends_df.loc[(friends_df["friendA"] == friend2)]["friendB"]
@@ -19,8 +21,9 @@ def find_common_friends(friend1,friend2):
 def nth_connection(friend1,friend2):
     if friend1 == friend2:
         return -1
+    
     visited = set()
-    queue = [(friend1,0)]
+    queue = [(friend1,0)]  #friend and level
     
     while queue:
         current_friend,level = queue.pop(0)
